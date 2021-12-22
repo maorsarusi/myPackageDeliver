9aac48ef5b3530173c5235c88ec5ecb06754c263
yosef <yamrusi@g.jct.ac.il>
2021-12-22 14:02:11 +0200

    add phone num

--- app/src/main/java/com/example/mypackagedeliver/Entities/User.kt
+++ app/src/main/java/com/example/mypackagedeliver/Entities/User.kt
@@ -1,4 +1,4 @@
 package com.example.mypackagedeliver.Entities
 
-data class User(val username:String,val password:String) {
+data class User(val username:String,val phoneNum:String,val password:String) {
 }
--- app/src/main/java/com/example/mypackagedeliver/UI/Login/registerActivity.kt
+++ app/src/main/java/com/example/mypackagedeliver/UI/Login/registerActivity.kt
@@ -16,17 +16,19 @@ class registerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         val userName = findViewById<EditText>(R.id.username)
+        val phoneNum = findViewById<EditText>(R.id.phoneNumber)
         val password = findViewById<EditText>(R.id.password)
 
         val button: Button = findViewById(R.id.register)
         button.setOnClickListener{
             val database = Firebase.database
-            if(!((userName.text.toString().equals(""))||password.text.toString().equals(""))){
+            if(!((userName.text.toString() == "")|| password.text.toString() == "" || phoneNum.text.toString() == "")){
                 val users_db = database.getReference("users")
                 val userNameString = userName.text.toString()
                 val passwordString=password.text.toString()
-                val user = insertIntoFireBase(userNameString,passwordString)
-                users_db.child(user.username).setValue(user)
+                val phoneNumString=phoneNum.text.toString()
+                val user = insertIntoFireBase(userNameString,phoneNumString,passwordString)
+                users_db.child(phoneNumString).setValue(user)
                 Toast.makeText(applicationContext, "The registration was successful", Toast.LENGTH_LONG).show()
                 val intent = Intent(this, loginActivity::class.java)
                 startActivity(intent)
@@ -40,9 +42,10 @@ class registerActivity : AppCompatActivity() {
     }
     private fun insertIntoFireBase(
         username:String,
+        phoneNum:String,
         password:String
     ): User {
-        return User(username,password)
+        return User(username,phoneNum,password)
 
     }
     private fun isNumber(s: String?): Boolean {
