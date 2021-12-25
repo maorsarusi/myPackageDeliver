02abc32767e2b5fba4764897057b8725b79c8a4b
yosef <yamrusi@g.jct.ac.il>
2021-12-25 18:28:48 +0200

    adding dependenci

--- app/src/main/java/com/example/mypackagedeliver/Entities/Parcel.kt
+++ app/src/main/java/com/example/mypackagedeliver/Entities/Parcel.kt
@@ -1,16 +1,31 @@
 package com.example.mypackagedeliver.Entities
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 /**
  * class that represent a package
  */
+@Entity(tableName = "parcel_data_table")
 data class Parcel
     (
+ @PrimaryKey(autoGenerate = true)
+ @ColumnInfo(name = "parcel_id")
  var pkg_id: Int,
+ @ColumnInfo(name = "owner")
  val owner : String,
+ @ColumnInfo(name = "owner_address")
  val owner_address : String,
+ @ColumnInfo(name = "type")
  val type : String,
+ @ColumnInfo(name = "weight")
  val weight : String,
+ @ColumnInfo(name = "lng")
  val lng : String,
+ @ColumnInfo(name = "lat")
  val lat : String,
+ @ColumnInfo(name = "fragile")
  val fragile : String,
+ @ColumnInfo(name = "phone")
  val phone : String)
---
+++ app/src/main/java/com/example/mypackagedeliver/Entities/ParcelDAO.kt
@@ -0,0 +1,14 @@
+package com.example.mypackagedeliver.Entities
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ParcelDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertParcel(parcel: Parcel)
+
+    //@Query(select * FROM parcel_data_table ORDER BY parcel_id ASC)
+    suspend fun readAllData():LiveData<List<Parcel>>
+}
